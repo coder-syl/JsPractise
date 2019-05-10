d8843a13187ad5e17d761e98ac6d6e63167470a0
coder-syl <2522011411@qq.com>
2019-05-11 00:20:09 +0800

    修复手风琴展开动画失效

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 1. 下拉导航栏
 
 	![下拉导航栏](demoImages/下拉导航栏.png)
+	
 2. 侧边栏
 
 	![侧边栏](demoImages/侧边栏.png)
@@ -14,4 +15,7 @@
 	![弹窗](demoImages/弹窗.png)
 
 5. 手风琴
+
+	![手风琴](demoImages/手风琴.png)
+	
 6. 轮播图
--- 手风琴/js/accord.js
+++ 手风琴/js/accord.js
@@ -1,17 +1,20 @@
-alert('nihao');
 var acc=document.getElementsByClassName('accordion');
 console.log(acc);
 for(let i=0;i<acc.length;i++){
     acc[i].onclick=function () {
-        console.log('dianji');
+        console.log('点击 的panel序号',i);
         acc[i].classList.toggle('active');
+        //获取相邻的下一个元素，即panel
         var panel=acc[i].nextElementSibling;
-        if(panel.style.maxHeight){
-            console.log('当前尺寸为！=0');
+        console.log(panel.style.maxHeight)
+        if(panel.style.maxHeight!=0){
             panel.style.maxHeight=null;
+            console.log('当前关闭的尺寸为',panel.style.maxHeight);
 
         }else{
-            panel.style.maxHeight="100%";
+            panel.style.maxHeight="500px";
+            console.log('当前打开的尺寸为', panel.style.maxHeight);
+
         }
     }
 
